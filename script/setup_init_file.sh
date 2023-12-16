f118e6aeaf76757fe2ebbda397b04182e4701b06
minjae <rlaalswo201@gmail.com>
2023-12-16 13:37:13 +0900

    Fix scritp/setup_init_file.sh

--- script/setup_init_file.sh
+++ script/setup_init_file.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 SETUP_DIR=$HOME/.local/share
 INIT_DIR=$HOME/.config/nvim
+PACKER_DIR=$HOME/.local/share/nvim/site/pack/packer/start
 PROJECT_ROOT=$(pwd)
 
 if [ ! -d $HOME/.config ] ;then
 	mkdir $HOME/.config
+fi
+
+if [ ! -d $PACKER_DIR ] ;then
+	mkdir -p $PACKER_DIR
+else
+	if [ -d $SETUP_DIR/nvim ] ;then
+    	rm -rf $SETUP_DIR/nvim
+	fi
 fi
 
 if [ -d $INIT_DIR ] ;then
