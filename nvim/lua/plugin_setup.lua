ef9e1c85b59a8f7622f8c33ec5a5890028dbb716
gamjagoon <rlaalswo201@gmail.com>
2024-03-17 13:57:26 +0900

    change snippet mapping

--- nvim/lua/plugin_setup.lua
+++ nvim/lua/plugin_setup.lua
@@ -2,6 +2,7 @@ local telescope = require("telescope")
 local lga_actions = require("telescope-live-grep-args.actions")
 local cmp = require('cmp')
 local autopair = require('nvim-autopairs')
+local luasnip = require("luasnip")
 
 require'nvim-treesitter.configs'.setup {
 	sync_install = false,
@@ -56,6 +57,12 @@ require('nvim-treesitter.parsers').get_parser_configs().asm = {
 
 require('telescope').load_extension('fzf')
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -69,11 +76,29 @@ cmp.setup({
 		{name = 'luasnip'},
 	}),
 	mapping = cmp.mapping.preset.insert({
-	  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-	  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-	  ['<C-Space>'] = cmp.mapping.complete(),
-	  ['<C-e>'] = cmp.mapping.abort(),
-	  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<C-e>'] = cmp.mapping.abort(),
+		["<Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+			-- that way you will only jump inside the snippet region
+		elseif luasnip.expand_or_jumpable() then
+			luasnip.expand_or_jump()
+		elseif has_words_before() then
+			cmp.complete()
+		else
+			fallback()
+		end end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+			cmp.select_prev_item()
+		  elseif luasnip.jumpable(-1) then
+			luasnip.jump(-1)
+		  else
+			fallback()
+		  end
+		end, { "i", "s" }),
 	}),
 })
 
